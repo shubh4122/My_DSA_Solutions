c629863b6ecf219a50f0015e16019400f91a5519
Shubham Pandey <shubhampandeylearning@gmail.com>
2022-09-18 23:52:58 +0530

    Sorted stack using recursion

--- Recursion.java
+++ Recursion.java
@@ -1,41 +1,92 @@
 package DSA;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Stack;
 
 public class Recursion {
     public static void main(String[] args) {
 //        print_1_to_n(10);
 //        print_n_to_1(10);
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(4);
-        arr.add(7);
-        arr.add(3);
-        arr.add(32);
-        arr.add(53);
-        arr.add(31);
-        arr.add(363);
-        arr.add(13);
-        arr.add(23);
-        arr.add(36);
-        arr.add(2);
-        arr.add(1);
-        arr.add(8);
-        arr.add(3);
-        System.out.println(sortArrRecursively(arr));
+
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        arr.add(4);
+//        arr.add(7);
+//        arr.add(3);
+//        arr.add(32);
+//        arr.add(53);
+//        arr.add(31);
+//        arr.add(363);
+//        arr.add(13);
+//        arr.add(23);
+//        arr.add(36);
+//        arr.add(2);
+//        arr.add(1);
+//        arr.add(8);
+//        arr.add(3);
+//        System.out.println(sortArrRecursively(arr));
+        Stack<Integer> s = new Stack<>();
+        s.push(4);
+        s.push(45);
+        s.push(1);
+        s.push(3);
+        s.push(8);
+        s.push(53);
+        s.push(24);
+
+        System.out.println(s);
+        sortStack(s);
+        System.out.println(s);
     }
 
 //----------Following problems are from Aditya verma, unless specified----------
 
+
+    //Similar to array
+    public static void sortStack(Stack<Integer> s) {
+//        H : sort(s) --> sorted stack
+//        I : sort(s) --> sort(stack except the top elem),
+//                          Insert top to the sorted Stack
+//        BC : if(s.size = 1) return;
+
+        if (s.size() == 1) return;
+
+        int top = s.pop();
+        sortStack(s);
+        insertInSortedStack(top, s);
+    }
+
+    private static void insertInSortedStack(int top, Stack<Integer> s) {
+//        H : insert(top, s) --> inserts top in 's' in a way that it keeps 's' sorted
+//        I : insert(top, s) --> Remove top' of this s.
+//                               insert the passed elem(top), Reinsert the top' to stack
+//        BC : if(s.size = 0 || s.peek < top) --> insert top to stack
+
+        if (s.size() == 0 || top > s.peek()) {
+            s.push(top);
+            return;
+        }
+
+        int topSortedStack = s.pop();
+        insertInSortedStack(top, s);
+        s.push(topSortedStack);
+    }
+
     public static ArrayList<Integer> sortArrRecursively(ArrayList<Integer> arr) {
         //Hypothesis - sort(arr) --> sorted arr
-        //Inductive - sort(arr) --> sort(arr.remove(last elem)) [i.e. leaving the last elem of arr]
+        //Inductive - sort(arr) --> sort(arr.remove(last elem)) [i.e. leaving the last elem of arr, we pass rest of the arr to sort function]
         //                                  and insert the last excluded elem in the sorted arr
         //Base Condition - if size = 1, return the only elem
 
+
+        //BC
         if (arr.size() == 1)
             return arr;
 
+
+//        Except the last elem, we pass rest of the arr
+//        and get the sorted arr in sortedArr
         int lastElem = arr.remove(arr.size() - 1);
         ArrayList<Integer> sortedArr = sortArrRecursively(arr);
 
@@ -52,21 +103,25 @@ public class Recursion {
 
         //-------------Inserting Recursively-------------
         insertInSortedArr(lastElem, sortedArr);
-
+//      inserts the last elem that was held back to the sorted arr we receive
         return sortedArr;
     }
 
-    private static void insertInSortedArr(int lastElem, ArrayList<Integer> sortedArr) {
-//        Hypothesis - insert(lastelem, arr) --> insert elem in sorted arr, so it remains sorted]
-//        Induction - insert(...) --> remove this arr last elem, if its < lastElem. insert lastElem. Reinsert previously removed elem.
-//        BC - if arr.size = 0 || arr.get(size - 1) <= lastElem --> push elem to back of arr
+    private static void insertInSortedArr(int elem, ArrayList<Integer> sortedArr) {
+//        Hypothesis - insert(elem, arr) --> insert elem in sorted arr, so it remains sorted
+//        Induction - insert(...) --> remove this arr last elem, if its < elem. insert elem. Reinsert previously removed elem.
+//        BC - if arr.size = 0 || arr.get(size - 1) <= elem --> push elem to back of arr
 
-        if (sortedArr.size() == 0 || sortedArr.get(sortedArr.size() - 1) <= lastElem) {
-            sortedArr.add(sortedArr.size(), lastElem);
+        if (sortedArr.size() == 0 || sortedArr.get(sortedArr.size() - 1) <= elem) {
+            sortedArr.add(sortedArr.size(), elem);
             return;
         }
+
+//        Removing the last elem of array, because its greater than the LastElem we get to insert
         int last = sortedArr.remove(sortedArr.size() - 1);
-        insertInSortedArr(lastElem, sortedArr);
+//        here we insert the elem(lastElem)
+        insertInSortedArr(elem, sortedArr);
+//        now we reinsert the elem we took out which was greater than lastElem
         sortedArr.add(sortedArr.size(), last);
     }
 
