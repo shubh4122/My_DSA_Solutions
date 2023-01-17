7e53d9e89196f534ca685b494dcc91584516b571
Shubham Pandey <shubhampandeylearning@gmail.com>
2023-01-18 02:15:21 +0530

    DP - Min subset sum Difference

--- DP.java
+++ DP.java
@@ -1,14 +1,175 @@
 package DSA;
 
 import java.util.Arrays;
+/*
 
+
+NOTE : Most Codes below are Top Down(Tabulation) Approach.
+       BUT if some new problem is encountered. always try writing recursive code, and then MEMOIZING it!!!
+
+
+ */
 public class DP {
     static int[][] dpMem = new int[1001][1001];//n=1000, w=1000
     public static void main(String[] args) {
         //initializing dp arr with -1, coz max profit cant be -1
         for (int i = 0; i < dpMem.length; i++)
             Arrays.fill(dpMem[i], -1);
+
+        int[] arr = {1, 4} ;//{2, 3, 5, 6, 8, 10};
+        int sum = 10;
+//        System.out.println(countSubsetsMem(arr, arr.length, sum, dpMem));
+        System.out.println(subsetPartitionMinSumDifference(arr, arr.length));
     }
+
+    public static int subsetPartitionMinSumDifference(int[] arr, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += arr[i];
+
+        int halfSum = sum/2;
+        //We can also use maxSumNearestToHalfSum. that works exactly like knapsack
+        int sumPartition1 = ksMem(arr, arr, halfSum, n);
+        int sumPartition2 = sum - sumPartition1;
+
+        return Math.abs(sumPartition2 - sumPartition1);
+    }
+
+    //This function returns SUM nearest to given sum
+    private static int maxSumNearestToHalfSum(int[] arr, int n, int sum) {
+        //BC
+        if (sum == 0 || n == 0)   return 0;
+
+        /*
+            M1 : Again write code similar to KnapSack.
+            or
+            M2 : Call knapsackMemoization with given values
+         */
+
+//        M1:
+//        Memoization
+        if (dpMem[n][sum] != -1)
+            return dpMem[n][sum];
+
+//        CODE(Choice Diag)
+//        Just like knapsack, Return Max Sum(profit there) nearest to Given SUM(W there)
+        if (arr[n-1] <= sum)
+            return dpMem[n][sum] = Math.max(arr[n-1] + maxSumNearestToHalfSum(arr, n-1, sum - arr[n-1]),
+                                            maxSumNearestToHalfSum(arr, n-1, sum));
+
+        else return dpMem[n][sum] = maxSumNearestToHalfSum(arr, n-1, sum);
+    }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //This is MY tabulation code, which is written by CONVERTING MY memoization code
+    public static int countSubsetTabulationMY(int[] arr, int n, int sum) {
+        int[][] dp = new int[n+1][sum+1];
+        //BC
+        Arrays.fill(dp[0], 0);
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n + 1; i++)
+            for (int j = 0; j < sum + 1; j++)
+                if (arr[i-1] <= j)
+                    //count of subsets equal to given sum, when including (n-1)th item in arr
+                    //+
+                    // count of subsets whose sum = given sum, when not including (n-t)th elem
+                    dp[i][j] = mod1000000007(dp[i-1][j - arr[i-1]] + dp[i-1][j]);
+
+                else    dp[i][j] = dp[i-1][j];
+
+
+        return dp[n][sum];
+    }
+
+    //this function returns count of subsets from arr of size n, whose sum = given sum
+    public static int countSubsetsMem(int[] arr, int n, int sum, int[][] dpMem) {
+        //BC
+        if (n == 0 && sum != 0)
+            return 0; //No subset possible. hence count = 0
+
+        if (n == 0 && sum == 0)
+            return 1;//empty set possible
+
+        //Memoization
+        if (dpMem[n][sum] != -1)
+            return dpMem[n][sum];
+
+        //Main code (Choice Diag)
+        if (arr[n-1] <= sum)
+            return dpMem[n][sum] = mod1000000007(countSubsetsMem(arr, n-1, sum - arr[n-1], dpMem) //count of subsets equal to given sum, when including (n-1)th item in arr
+                                +
+                    countSubsetsMem(arr, n - 1, sum, dpMem)); // count of subsets whose sum = given sum, when not including (n-t)th elem
+
+        else    return dpMem[n][sum] = mod1000000007(countSubsetsMem(arr, n - 1, sum, dpMem));
+    }
+
+    public static int countSubsetsTabulation(int[] arr, int n, int sum) { //count subsets with given sum
+        int[][] dp = new int[n+1][sum+1];
+        int count0 = 0;
+
+        //Initialization - row0 -> F = 0, col0 -> T = 1
+        Arrays.fill(dp[0], 0);//row0 filled
+        dp[0][0] = 1;
+        for (int i = 1; i < n + 1; i++) {
+            if (arr[i-1] == 0)  count0++;
+            dp[i][0] = (int)Math.pow(2, count0); // col0 filled
+        }
+
+        //Choice Diag code
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i-1] <= j)
+                    dp[i][j] = mod1000000007(dp[i - 1][j - arr[i - 1]] + dp[i - 1][j]);//||(or) replaced by +
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return mod1000000007(dp[n][sum]);
+    }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public static int equalPartition(int n, int arr[]) {
+        //Return 1. For true.
+        //Step 1: Find Total sum of arr
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        //Step 2: If sum - Odd, then no partition can be done, If Even - partition MAY BE possible
+        if (sum%2 != 0) //odd sum
+            return 0;   //Partition NOT possible
+
+        /*
+            We can Directly call subset sum function with sum/2 ALSO!!!
+            OR
+            write its code again!
+         */
+
+        //Step 3: Now Start DP.
+        return isSubsetSum(n, arr, sum/2) ? 1 : 0;
+//        boolean[][] dp = new boolean[n+1][(sum/2)+1];
+//        //Initialization
+//        Arrays.fill(dp[0], false);//row0 filled
+//        for (int i = 0; i < n + 1; i++)
+//            dp[i][0] = true; // col0 filled
+//
+//        //Choice Diag code ---------- BE VERY CAREFULLLLL!!!!  i and j start from 1 !!!!!!!!!!!!!!
+//        for (int i = 1; i < n + 1; i++) {
+//            for (int j = 1; j < (sum / 2) + 1; j++) {
+//                if (arr[i-1] <= j)
+//                    dp[i][j] = dp[i-1][j - arr[i-1]] || dp[i-1][j];
+//
+//                else
+//                    dp[i][j] = dp[i-1][j];
+//            }
+//        }
+//        return dp[n][sum/2] ? 1 : 0;
+    }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     public static boolean isSubsetSum(int n, int arr[], int sum) {
         boolean[][] dp = new boolean[n+1][sum+1];
@@ -18,7 +179,7 @@ public class DP {
             dp[i][0] = true; // col0 filled
         }
 
-        //Choice Diag code
+        //Choice Diag code ---------- BE VERY CAREFULLLLL!!!!  i and j start from 1 !!!!!!!!!!!!!!
         //NOTE: The loop MUST START FROM 1, 0th row, col have already been taken care of
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < sum + 1; j++) {
@@ -30,6 +191,8 @@ public class DP {
         }
         return dp[n][sum];
     }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     public static int ksTab(int[] val, int[] wt, int w, int n) {
         int[][] dp = new int[n+1][w+1];
@@ -50,6 +213,7 @@ public class DP {
         return dp[n][w];
     }
 
+    //This function returns the Max Profit that can be done while filling W wt.
     public static int ksMem(int[] val, int[] wt, int w, int n) {
         //BC
         if (n == 0 || w == 0)
@@ -66,5 +230,11 @@ public class DP {
 
         else //if wt[n-1] > w
             return dpMem[n][w] = ksMem(val, wt, w, n-1);
+    }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    public static int mod1000000007(long n) {
+        return (int)(n%(1e9+7));
     }
 }
