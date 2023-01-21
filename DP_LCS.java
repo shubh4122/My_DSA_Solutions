551a50b26c3e67b64d3c51cb2c50f2c191cf7462
Shubham Pandey <shubhampandeylearning@gmail.com>
2023-01-22 01:25:56 +0530

    DP - Longetst Common SUBSTRING

--- DP_LCS.java
+++ DP_LCS.java
@@ -1,6 +1,7 @@
 package DSA;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class DP_LCS {
     public static void main(String[] args) {
@@ -16,6 +17,58 @@ public class DP_LCS {
         System.out.println(lcs(lx, ly, x, y, dpMem));
     }
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public static int longestCommonSUBSTRING_Tabulation(int lx, int ly, String x, String y) {
+        //table
+        int[][] dp = new int[lx+1][ly+1];
+
+        //Initialization
+        Arrays.fill(dp[0], 0);
+        for (int i = 0; i < lx + 1; i++)
+            dp[i][0] = 0;
+
+        //Choice Diagram
+        for (int i = 1; i < lx + 1; i++) {
+            for (int j = 1; j < ly + 1; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else //last char doesn't matches
+                    dp[i][j] = 0;
+            }
+        }
+
+        //Now, here we need to find what is the MAX val in Matrix
+        //Here we dont simply return dp[lx][ly]
+        int maxLen = -1;
+        for (int i = 0; i < lx + 1; i++)
+            for (int j = 0; j < ly + 1; j++)
+                if (maxLen < dp[i][j])
+                    maxLen = dp[i][j];
+
+        return maxLen;
+    }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public static int lcsTabulation(int lx, int ly, String x, String y) {
+        //table
+        int[][] dp = new int[lx+1][ly+1];
+
+        //Initialization
+        Arrays.fill(dp[0], 0);
+        for (int i = 0; i < lx + 1; i++)
+            dp[i][0] = 0;
+
+        //Choice Diagram
+        for (int i = 1; i < lx + 1; i++) {
+            for (int j = 1; j < ly + 1; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else //last char doesn't matches
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+
+        return dp[lx][ly];
+    }
+
     public static int lcs(int lx, int ly, String x, String y, int[][] dpMem) {
 //        Hypothesis --> lcs returns the len of common subsequence between
 //                       2 given strings. And we ensure it is the longest.
